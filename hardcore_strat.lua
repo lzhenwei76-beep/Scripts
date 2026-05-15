69656e81e961fa2da3e0f79201f5b8a143978556
lzhenwei <lzhenwei76@gmail.com>
2026-05-15 20:54:47 +0200

    Create hardcore_strat.lua

---
+++ hardcore_strat.lua
@@ -0,0 +1,211 @@
+local TDS = loadstring(game:HttpGet("https://raw.githubusercontent.com/DuxiiT/auto-strat/refs/heads/main/Library.lua"))()
+
+TDS:Loadout("Gatling Gun", "DJ Booth", "Cowboy", "Ranger", "Ace Pilot")
+TDS:Mode("Hardcore")
+TDS:GameInfo("Crossroads", {})
+
+TDS:Place("Cowboy", 4.240627, 0.999999, 7.870407)
+
+-- [ Wave 1 ] --
+TDS:Upgrade(1)
+
+-- [ Wave 3 ] --
+TDS:Place("Cowboy", 6.848572, 0.999999, 7.681025)
+TDS:Upgrade(2)
+
+-- [ Wave 5 ] --
+TDS:Place("Cowboy", 5.787426, 0.999998, 10.200583, true)
+TDS:Upgrade(3)
+
+-- [ Wave 6 ] --
+TDS:Place("Cowboy", 8.001781, 0.999998, 9.903666, true)
+TDS:Upgrade(4)
+
+-- [ Wave 7 ] --
+TDS:Place("Cowboy", 9.442945, 0.999999, 7.511804, true)
+TDS:Place("Cowboy", 11.75812, 0.999999, 8.065916, true)
+
+-- [ Wave 8 ] --
+TDS:Upgrade(5)
+TDS:Upgrade(6)
+
+-- [ Wave 9 ] --
+TDS:Upgrade(4)
+
+-- [ Wave 10 ] --
+TDS:Upgrade(3)
+
+-- [ Wave 12 ] --
+TDS:Place("Ace Pilot", 5.232809, 0.999998, 12.863498, true)
+TDS:Place("Ace Pilot", 6.731525, 0.999998, 12.908741, true)
+TDS:Upgrade(7)
+TDS:Upgrade(8)
+
+-- [ Wave 13 ] --
+TDS:Place("Ace Pilot", 7.989156, 0.999998, 12.671329, true)
+TDS:Upgrade(9)
+TDS:Upgrade(7)
+
+-- [ Wave 14 ] --
+TDS:Upgrade(8)
+TDS:Upgrade(9)
+
+-- [ Wave 15 ] --
+TDS:Upgrade(8)
+
+-- [ Wave 16 ] --
+TDS:Upgrade(9)
+
+-- [ Wave 17 ] --
+TDS:Upgrade(7)
+
+-- [ Wave 19 ] --
+TDS:Upgrade(8)
+TDS:Upgrade(7)
+
+-- [ Wave 21 ] --
+TDS:Upgrade(9)
+
+-- [ Wave 23 ] --
+TDS:Place("Gatling Gun", 20.688547, 12.663231, 20.311642)
+
+-- [ Wave 24 ] --
+TDS:Upgrade(10)
+
+-- [ Wave 26 ] --
+TDS:Upgrade(10)
+
+-- [ Wave 27 ] --
+TDS:Place("DJ Booth", 12.519014, 0.999998, 11.923302)
+TDS:Upgrade(11)
+TDS:Upgrade(11)
+
+-- [ Wave 30 ] --
+TDS:Upgrade(10)
+
+-- [ Wave 34 ] --
+TDS:Place("Ranger", 24.624849, 12.663228, 21.619396, true)
+TDS:Place("Ranger", 25.055555, 12.66323, 20.285074, true)
+TDS:Place("Ranger", 25.38036, 12.66323, 20.269638, true)
+TDS:Place("Ranger", 24.528404, 12.663231, 19.990503, true)
+TDS:Place("Ranger", 24.703012, 12.66323, 20.198793, true)
+TDS:Place("Ranger", 24.859861, 12.66323, 20.224756, true)
+TDS:Place("Ranger", 25.22707, 12.66323, 20.243452, true)
+TDS:Place("Ranger", 25.306084, 12.66323, 20.405922, true)
+TDS:Upgrade(12)
+TDS:Upgrade(13)
+TDS:Upgrade(14)
+TDS:Upgrade(15)
+TDS:Upgrade(16)
+
+-- [ Wave 35 ] --
+TDS:Upgrade(19)
+TDS:Upgrade(18)
+TDS:Upgrade(13)
+TDS:Upgrade(17)
+
+-- [ Wave 36 ] --
+TDS:Upgrade(11)
+TDS:Upgrade(11)
+TDS:Upgrade(12)
+TDS:Upgrade(14)
+TDS:Upgrade(19)
+TDS:Upgrade(18)
+TDS:Upgrade(16)
+TDS:Upgrade(15)
+TDS:Upgrade(17)
+TDS:Upgrade(1)
+
+-- [ Wave 37 ] --
+TDS:Upgrade(2)
+TDS:Upgrade(5)
+TDS:Upgrade(6)
+TDS:Upgrade(2)
+TDS:Upgrade(3)
+TDS:Upgrade(1)
+TDS:Upgrade(4)
+TDS:Upgrade(5)
+TDS:Upgrade(6)
+
+-- [ Wave 38 ] --
+TDS:Upgrade(10)
+
+-- [ Wave 41 ] --
+TDS:Upgrade(10)
+TDS:Upgrade(9)
+
+-- [ Wave 42 ] --
+TDS:Upgrade(8)
+TDS:Upgrade(7)
+TDS:Upgrade(12)
+
+-- [ Wave 43 ] --
+TDS:Upgrade(15)
+TDS:Upgrade(16)
+TDS:Upgrade(17)
+
+-- [ Wave 44 ] --
+TDS:Upgrade(14)
+TDS:Upgrade(18)
+TDS:Upgrade(13)
+TDS:Upgrade(19)
+
+-- [ Wave 45 ] --
+TDS:Upgrade(10)
+
+-- [ Wave 46 ] --
+TDS:Upgrade(12)
+TDS:Upgrade(13)
+TDS:Upgrade(14)
+TDS:Upgrade(15)
+TDS:Upgrade(16)
+TDS:Upgrade(19)
+
+-- [ Wave 47 ] --
+TDS:Upgrade(17)
+TDS:Upgrade(18)
+TDS:Upgrade(1)
+TDS:Upgrade(2)
+TDS:Upgrade(3)
+TDS:Upgrade(4)
+TDS:Upgrade(5)
+TDS:Upgrade(6)
+TDS:Upgrade(11)
+TDS:Upgrade(1)
+TDS:Upgrade(2)
+TDS:Upgrade(3)
+TDS:Upgrade(4)
+TDS:Upgrade(5)
+TDS:Upgrade(6)
+TDS:Place("Ace Pilot", 9.102989, 0.999998, 14.46554, true)
+TDS:Place("Ace Pilot", 8.435115, 0.999997, 16.039286, true)
+TDS:Place("Ace Pilot", 8.102591, 0.999997, 16.133837, true)
+TDS:Place("Ace Pilot", 7.843171, 0.999997, 16.253454, true)
+
+-- [ Wave 48 ] --
+TDS:Place("Ace Pilot", 7.470841, 0.999997, 16.035978, true)
+TDS:Upgrade(20)
+TDS:Upgrade(21)
+TDS:Upgrade(22)
+TDS:Upgrade(23)
+TDS:Upgrade(24)
+TDS:Upgrade(20)
+TDS:Upgrade(21)
+TDS:Upgrade(22)
+TDS:Upgrade(23)
+TDS:Upgrade(24)
+TDS:Upgrade(20)
+TDS:Upgrade(21)
+TDS:Upgrade(22)
+TDS:Upgrade(23)
+TDS:Upgrade(24)
+TDS:Upgrade(20)
+TDS:Upgrade(21)
+TDS:Upgrade(22)
+TDS:Upgrade(23)
+TDS:Upgrade(24)
+TDS:Upgrade(20)
+TDS:Upgrade(21)
+TDS:Upgrade(22)
+TDS:Upgrade(23)
+TDS:Upgrade(24)
